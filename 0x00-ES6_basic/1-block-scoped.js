ab7cb9cc3d9305a1d3ce0f270c2c289c7ab4eecc
Ano-ly <anomalie23.0@gmail.com>
2024-05-28 05:16:11 +0100

    Edit task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false; // eslint-disable-line no-unused-vars
-  var task2 = true; // eslint-disable-line no-unused-vars
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
