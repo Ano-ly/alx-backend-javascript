431ce085032387702199fde0cd90fa75aaf957dc
Ano-ly <anomalie23.0@gmail.com>
2024-05-28 05:10:32 +0100

    Task 4

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,7 @@
+export default function returnHowManyArguments (...myArgs) {
+  let counter = 0;
+  for (const item of myArgs) { // eslint-disable-line no-unused-vars
+    counter += 1;
+  }
+  return counter;
+}
