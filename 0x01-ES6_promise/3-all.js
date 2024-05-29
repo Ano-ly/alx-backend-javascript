92d0eb69770cade2e95b151e3fb2abd5382e1f97
Ano-ly <anomalie23.0@gmail.com>
2024-05-29 09:14:52 +0100

    Task 3 edit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup () {
-  const itemOne = uploadPhoto();
-  const itemTwo = createUser();
-  itemOne.then(function (result1) {
-    itemTwo.then(function (result2) {
-      console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
-    }).catch(function () {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
       console.log('Signup system offline');
     });
-  }).catch(function () {
-    console.log('Signup system offline');
-  });
 }
