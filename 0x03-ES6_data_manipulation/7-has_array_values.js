c180020f8ca155bf434baecac29f942111c8f1c9
Ano-ly <anomalie23.0@gmail.com>
2024-06-05 05:39:54 +0100

    Complete task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,20 +1,3 @@
-export default function getListStudents() {
-  const myArray = [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
-  return myArray;
+export default function hasValuesFromArray(mySet, myArray) {
+  return myArray.every((element) => mySet.has(element));
 }
