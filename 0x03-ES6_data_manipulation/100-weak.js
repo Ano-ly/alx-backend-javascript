b193a6650d9ac58699e5730852e71e85865f6128
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-06 18:52:30 +0100

    Advanced task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,11 @@
-let endpoint = 0;
+let end = 0;
 export const weakMap = new WeakMap();
+
 export function queryAPI(myObj) {
-  weakMap.set(endpoint, ++endpoint); 
+  end++;
+  weakMap.set(myObj, end);
+
+  if (weakMap.get(myObj) >= 5) {
+   throw new Error("Endpoint load is high");
+  }
 }
