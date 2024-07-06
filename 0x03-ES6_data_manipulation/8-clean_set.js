e8645d66378a15db13d393ac627906984d4bdb24
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-06 19:13:21 +0100

    Task 8 edit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,14 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString === 'undefined') {
+    return '';
+  }
   if (startString.length === 0) {
     return '';
   }
   const strLen = startString.length;
   const newArray = [];
   for (const myStr of set) {
-    if (typeof(myStr) === 'undefined') {
+    if (typeof myStr === 'undefined') {
       continue;
     }
     if (myStr.startsWith(startString) && myStr.length > strLen) {
