3e7cd31a47ae3149da81db8c3ff6328b20472563
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-23 19:17:18 +0100

    Task 2 edit

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,14 +6,30 @@ function countStudents(myPath) {
       throw new Error('Cannot load the database\n');
     } else {
       const str = mystr.trim();
-      const myArray = str.split('\n').slice(1);
+      const array = str.split('\n');
+      const cols = array[0].split(',')
+      const myArray = array.slice(1);
+      function findName(el) {
+        if (el === 'firstname') {
+          return true;
+        }
+        return false;
+      }
+      function findField(el) {
+        if (el === 'field') {
+          return true;
+        }
+        return false;
+      }
+      nameIdx = cols.findIndex(findName);
+      fieldIdx = cols.findIndex(findField);
       const lenStuds = myArray.length;
       console.log(`Number of students: ${lenStuds}`);
 
       const listMajor = new Set();
       for (const myItem of myArray) {
         const params = myItem.split(',');
-        listMajor.add(params.slice(-1)[0]);
+        listMajor.add(params[fieldIdx]);
       }
 
       for (const major of listMajor) {
@@ -21,8 +37,8 @@ function countStudents(myPath) {
         const names = [];
         for (const item of myArray) {
           const infoList = item.split(',');
-          if (major === infoList.slice(-1)[0]) {
-            names.push(infoList[0]);
+          if (major === infoList[fieldIdx]) {
+            names.push(infoList[nameIdx]);
             noStuds++;
           }
         }
