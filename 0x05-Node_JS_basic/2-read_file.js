aff3026144a3d17d3b63a08c9da6c94e6ca9cb2a
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-27 08:03:39 +0100

    Task 2 edit

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -15,40 +15,40 @@ function findField(el) {
 }
 
 function countStudents(myPath) {
-  fs.readFile(myPath, { encoding: 'utf-8' }, (err, mystr) => {
-    if (err) {
-      throw new Error('Cannot load the database\n');
-    } else {
-      const str = mystr.trim();
-      const array = str.split('\n');
-      const cols = array[0].split(',');
-      const myArray = array.slice(1);
-      const nameIdx = cols.findIndex(findName);
-      const fieldIdx = cols.findIndex(findField);
-      const lenStuds = myArray.length;
+  try {
+    theStr = fs.readFileSync(myPath, { encoding: 'utf-8' })   
+    const str = theStr.trim();
+    const array = str.split('\n');
+    const cols = array[0].split(',');
+    const myArray = array.slice(1);
+    const nameIdx = cols.findIndex(findName);
+    const fieldIdx = cols.findIndex(findField);
+    const lenStuds = myArray.length;
 
-      const listMajor = new Set();
-      for (const myItem of myArray) {
-        const params = myItem.split(',');
-        listMajor.add(params[fieldIdx]);
-      }
-      console.log(`Number of students: ${lenStuds}`);
-
-      for (const major of listMajor) {
-        let noStuds = 0;
-        const names = [];
-        for (const item of myArray) {
-          const infoList = item.split(',');
-          if (major === infoList[fieldIdx]) {
-            names.push(infoList[nameIdx]);
-            noStuds += 1;
-          }
-        }
-        const studs = names.join(', ');
-        console.log(`Number of students in ${major}: ${noStuds}. List: ${studs}`);
-      }
+    const listMajor = new Set();
+    for (const myItem of myArray) {
+      const params = myItem.split(',');
+      listMajor.add(params[fieldIdx]);
     }
-  });
+    console.log(`Number of students: ${lenStuds}`);
+
+    for (const major of listMajor) {
+      let noStuds = 0;
+      const names = [];
+      for (const item of myArray) {
+        const infoList = item.split(',');
+        if (major === infoList[fieldIdx]) {
+          names.push(infoList[nameIdx]);
+          noStuds += 1;
+        }
+      }
+      const studs = names.join(', ');
+      console.log(`Number of students in ${major}: ${noStuds}. List: ${studs}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database\n');
+  }
 }
 
 module.exports = countStudents;
+
