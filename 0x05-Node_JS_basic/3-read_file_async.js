abb710a35762ff3150f3dbd1924e18f59e1b5cbc
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-27 08:18:15 +0100

    Task 3 lint error

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,12 +1,53 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const fs = require('fs');
 
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
+function findName(el) {
+  if (el === 'firstname') {
+    return true;
   }
-});
+  return false;
+}
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+function findField(el) {
+  if (el === 'field') {
+    return true;
+  }
+  return false;
+}
+
+function countStudents(myPath) {
+  fs.readFile(myPath, { encoding: 'utf-8' }, (err, theStr) => {
+    if (err) {
+      throw new Error('Cannot load the database\n');
+    }
+    const str = theStr.trim();
+    const array = str.split('\n');
+    const cols = array[0].split(',');
+    const myArray = array.slice(1);
+    const nameIdx = cols.findIndex(findName);
+    const fieldIdx = cols.findIndex(findField);
+    const lenStuds = myArray.length;
+
+    const listMajor = new Set();
+    for (const myItem of myArray) {
+      const params = myItem.split(',');
+      listMajor.add(params[fieldIdx]);
+    }
+    console.log(`Number of students: ${lenStuds}`);
+
+    for (const major of listMajor) {
+      let noStuds = 0;
+      const names = [];
+      for (const item of myArray) {
+        const infoList = item.split(',');
+        if (major === infoList[fieldIdx]) {
+          names.push(infoList[nameIdx]);
+          noStuds += 1;
+        }
+      }
+      const studs = names.join(', ');
+      console.log(`Number of students in ${major}: ${noStuds}. List: ${studs}`);
+    }
+  });
+}
+
+module.exports = countStudents;
