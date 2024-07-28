f5c92c7830ec1496b93addf04bd89e46a7655907
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-28 08:11:06 +0100

    Task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,10 +1,87 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
 const port = 1245;
+
+args = process.argv;
+file = args[2];
+
+
+function findName(el) {
+  if (el === 'firstname') {
+    return true;
+  }
+  return false;
+}
+
+function findField(el) {
+  if (el === 'field') {
+    return true;
+  }
+  return false;
+}
+
+function countStudents(myPath) {
+  const myPromise = new Promise((resolve, reject) => {
+    const fieldsInfo = []
+    fs.readFile(myPath, { encoding: 'utf-8' }, (err, theStr) => {
+      if (err) {
+        reject(new Error('Cannot load the database\n'));
+      } else {
+        const str = theStr.trim();
+        const array = str.split('\n');
+        const cols = array[0].split(',');
+        const myArray = array.slice(1);
+        const nameIdx = cols.findIndex(findName);
+        const fieldIdx = cols.findIndex(findField);
+        const lenStuds = myArray.length;
+
+        const listMajor = new Set();
+        for (const myItem of myArray) {
+          const params = myItem.split(',');
+          listMajor.add(params[fieldIdx]);
+        }
+        intro = `Number of students: ${lenStuds}`;
+
+        for (const major of listMajor) {
+          let noStuds = 0;
+          const names = [];
+          for (const item of myArray) {
+            const infoList = item.split(',');
+            if (major === infoList[fieldIdx]) {
+              names.push(infoList[nameIdx]);
+              noStuds += 1;
+            }
+          }
+          const studs = names.join(', ');
+          fieldsInfo.push(`Number of students in ${major}: ${noStuds}. List: ${studs}`);
+          const fieldsStr = fieldsInfo.join('\n');
+        }
+        resolve(`${intro}\n${fieldsInfo.join('\n')}`);
+      }
+    });
+  });
+  return myPromise;
+}
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  myurl = url.parse(req.url).pathname;
+  if (myurl === '/') {
+    res.end('Hello Holberton School!');
+  } else if ( myurl === '/students') {
+    countStudents(file)
+      .then((str) => {
+        res.end(str);
+      })
+      .catch((err) => {
+        res.end(err);
+      })
+  } else {
+    res.end('Noe');
+  }
 });
 
 app.listen(port, () => {
